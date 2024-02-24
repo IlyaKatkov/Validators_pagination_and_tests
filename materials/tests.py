aa7207925c55b1728fddd96b781b3b6439ae176f
Ilya <ilya-katkov-95@mail.ru>
2024-02-24 21:20:00 +0300

    Validators_pagination_and_tests_3

--- materials/tests.py
+++ materials/tests.py
@@ -3,11 +3,11 @@ from rest_framework import status
 from materials.models import Course, Lesson
 from users.models import User
 from django.urls import reverse
-from django.test import TestCase
 from django.contrib.auth.models import Group
+from rest_framework.test import APITestCase
 
 
-class CourseTestCase(TestCase):
+class CourseTestCase(APITestCase):
     def setUp(self):
         self.moderator = User.objects.create(email='1@1.com', password='123123')
         group = Group.objects.create(name='moderator')
@@ -18,21 +18,21 @@ class CourseTestCase(TestCase):
         self.user.save()
 
     def test_course_create(self):
-        data = {"title": "course1", "description": "desc1"}
-        url = reverse('lms:course-list')
+        data = {"name": "course1", "description": "desc1"}
+        url = reverse('materials:course-list')
         self.client.force_login(user=self.moderator)
         response = self.client.post(url, data=data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_course_destroy(self):
-        course = Course.objects.create(title="course1", description="desc1", owner=self.user)
-        url = reverse('lms:course-detail', kwargs={'pk': course.pk})
+        course = Course.objects.create(name="course1", description="desc1", owner=self.user)
+        url = reverse('materials:course-detail', kwargs={'pk': course.pk})
         self.client.force_login(user=self.user)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_get_list_lesson(self):
-        self.client.force_authenticate(owner=self.user)
+        self.client.force_authenticate(user=self.user)
         responce = self.client.get('/lesson/')
         self.assertEquals(responce.status_code,status.HTTP_200_OK)
 
@@ -44,20 +44,20 @@ class CourseTestCase(TestCase):
             "video_link": "youtube.com",
         }
         print(data)
-        self.client.force_authenticate(owner=self.user)
+        self.client.force_authenticate(user=self.user)
         responce = self.client.post('/course/', data=data)
         print(responce)
         self.assertEquals(responce.status_code, status.HTTP_201_CREATED)
 
     def test_delete_lesson(self):
-        self.client.force_authenticate(owner=self.user)
+        self.client.force_authenticate(user=self.user)
         responce = self.client.delete('/lesson/delete/1/')
         print(Lesson.objects.all())
 
         self.assertEquals(responce.status_code, status.HTTP_204_NO_CONTENT)
 
 
-class SubscriptionAPITest(TestCase):
+class SubscriptionAPITest(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create(id=1, email="testuser@test.com", phone="123456", city="Moscow", password='123456')
